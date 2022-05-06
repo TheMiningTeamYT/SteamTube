8f2e0a495fa4fbf25b2522c440b168674149416f
TheMiningTeamYT <31364192+TheMiningTeamYT@users.noreply.github.com>
2022-05-05 21:53:49 -0500

    v0.50b - Midway Through!
    We're continuing on our steam-powered journey with this release!
    In this release, I've
    1: Tuned the speed at which the video and audio quality degrade
    2: Made the page UI more faithful to the original
    3: Added some text explaining everything!

--- main.js
+++ main.js
@@ -26,95 +26,88 @@ pressure = 0;
 pressureVenting = false;
 overheating = false;
 peakQuality = false;
-
 // Attach all the elements I need to variables.
 videoQualityControl = document.getElementById("videoQualityControl");
 audioQualityControl = document.getElementById("audioQualityControl");
-
+pressureVentButton = document.getElementById("pressureVent");
+// Make sure you don't make the player's ear's bleed.
+document.getElementById("audioNoise").volume = 0.25;
 // Start the function that builds up pressure
 pressureBuildup();
-
 // Add event listeners for the vent pressure button.
-document.getElementById("pressureVent").addEventListener("mousedown", function() {
+pressureVentButton.addEventListener("mousedown", function() {
   pressureVenting = true;
+  pressureVentButton.src = "assets/bigButtonPressed.png";
 })
-document.getElementById("pressureVent").addEventListener("mouseup", function() {
+pressureVentButton.addEventListener("mouseup", function() {
   pressureVenting = false;
+  pressureVentButton.src = "assets/bigButton.png";
+})
+// Make sure if the user doesn't release the mouse button while over the vent button, the vent button doesn't stay down.
+pressureVentButton.addEventListener("mouseout", function() {
+  if (pressureVenting === true) {
+    pressureVenting = false;
+    pressureVentButton.src = "assets/bigButton.png";
+  }
 })
 
 // Add event listener for the load button.
 document.getElementById("loadButton").addEventListener("click", function(){
-    document.getElementById("noise").style.zIndex = "2"
-    // Stupid YouTube API won't accept normal YouTube URLs for loadVideoByUrl, so I have to do this instead.
-    
-    var videoid = document.getElementById("videoUrl").value.replace(/https\:\/\/|www.youtube.com\/watch\?v=|www.youtu.be\//g, "")
-    
-    // Thanks Leo and Samina Zahid of Stack Overflow!
-    var remove_after = videoid.indexOf("&");
-    if (remove_after != -1) {
-        videoid = videoid.substring(0, remove_after);
-    }
-    
-    // Load video
-    player.loadVideoById({videoId: videoid});
-    
-    // Reset audio and video quality
-    videoQuality = 0;
-    audioQuality = 0;
-    })
-
+  document.getElementById("loadButton").src = "assets/littleButtonPressed.png";
+  var loadButtonAnimationTimeout = setTimeout(animateLoadButton, 200);
+  // Stupid YouTube API won't accept normal YouTube URLs for loadVideoByUrl, so I have to do this instead.
+  var videoid = document.getElementById("videoUrl").value.replace(/https\:\/\/|www.youtube.com\/watch\?v=|www.youtu.be\//g, "");
+  // Thanks Leo and Samina Zahid of Stack Overflow!
+  var remove_after = videoid.indexOf("&");
+  if (remove_after != -1) {
+      videoid = videoid.substring(0, remove_after);
+  }
+  // Load video
+  player.loadVideoById({videoId: videoid});
+  // Reset audio and video quality
+  videoQuality = 0;
+  audioQuality = 0;
+})
 // Start function to check the video quality.
 videoQualityCheck();
-
 // Define function to check the video quality.
 function videoQualityCheck() {
   // Check the video quality again in ~1/30 secs.
-	var videoQualityCheckTimeout = setTimeout(videoQualityCheck, 33)
-
+	var videoQualityCheckTimeout = setTimeout(videoQualityCheck, 33);
   // Make sure the record scratch-esque sound is playing
   document.getElementById("recordSound").play();
-	
 	// Get the current rotation of the video control.
   var rotation = getRotation(videoQualityControl);
-
   // Get the current rotation of the audio control.
   var audioQualityControlRotation = getRotation(audioQualityControl);
-
   // If you've turned the control wheel, play a ticking sound.
   if ((rotation != lastRotationVideo) || (audioQualityControlRotation != lastRotationAudio)) {
     document.getElementById("tickingSound").play();
   } else {
     document.getElementById("tickingSound").pause();
   }
-  
   // If you've rotated the control all the way around, add 360 to the rotation in the future.
-  if (rotation < (lastRotationVideo - 300)) {
+  if (rotation < (lastRotationVideo - 150)) {
       if (videoQualityControl.dataset.active == "true") {
           videoQuality += 360;
       }
   }
-  
   // If you've rotated the control backwards, don't count it.
-  if (rotation > (lastRotationVideo + 300)) {
+  if (rotation > (lastRotationVideo + 150)) {
       rotation -= rotation;
   }
-  
   // Save the current rotation of the control for future reference.
   lastRotationVideo = getRotation(videoQualityControl);
-  
   // Cap the video quality to 3200.
   if (videoQuality > 3200) {
       videoQuality = 3200;
   }
-  
   // If the video quality isn't already 0, slowly bring it down.
   if (videoQuality > 0) {
-      videoQuality -= ((1.0008 ** videoQuality) + 2)/1.5;
+      videoQuality -= ((1.0009 ** videoQuality) + 2)/1.5;
   }
-  
   // Set the opacity of the video noise according to the video quality.
   document.getElementById("noise").style.opacity = (1 -((rotation + videoQuality) / 2600));
-  
   // If you've maxed out the video quality, don't rotate the dial any further.
   if ((rotation + videoQuality) > 3200) {
     document.getElementById("videoDial").style.transform = "rotate(" + ((3200 / 13) - 120) + "deg)";
@@ -122,61 +115,48 @@ function videoQualityCheck() {
     // Otherwise, rotate the dial accordingly.
     document.getElementById("videoDial").style.transform = "rotate(" + (((rotation + videoQuality) / 13) - 120) + "deg)";
   }
-
   // Finally, check if you're at peak quality.
   if (peakQuality === false) {
     if (((videoQuality + rotation) >= 2600) && ((audioQuality + audioQualityControlRotation) >= 2600)) {
       peakQuality = true;
       document.getElementById("peakQualityLight").src = "assets/peakQuality.png"
-      document.getElementById("peakQualityLight").style.width = "162px"
     }
   } else if (peakQuality === true) {
     if (((videoQuality + rotation) < 2600) && ((audioQuality + audioQualityControlRotation) < 2600)) {
       peakQuality = false;
       document.getElementById("peakQualityLight").src = "assets/notPeakQuality.png"
-      document.getElementById("peakQualityLight").style.width = "168px"
     }
   }
   
 }
-
 // Start the function to check the audio quality.
 audioQualityCheck();
-
 // Define function to check the audio quality.
 function audioQualityCheck() {
-  
   // Check the audio quality again in ~1/30 secs.
-	var audioQualityCheckTimeout = setTimeout(audioQualityCheck, 33)
-	
+	var audioQualityCheckTimeout = setTimeout(audioQualityCheck, 33);
 	// Get the current rotation of the audio quality control.
   var rotation = getRotation(audioQualityControl);
-  
   // If you've rotated the control all the way around, add 360 to the rotation in the future.
-  if (rotation < (lastRotationAudio - 300)) {
+  if (rotation < (lastRotationAudio - 150)) {
       if (audioQualityControl.dataset.active == "true") {
           audioQuality += 360;
       }
   }
-  
   // If you've rotated the control backwards, don't count it.
-  if (rotation > (lastRotationAudio + 300)) {
+  if (rotation > (lastRotationAudio + 150)) {
       rotation -= rotation;
   }
-  
   // Save the current rotation of the control for future reference.
   lastRotationAudio = getRotation(audioQualityControl);
-  
   // Cap the audio quality to 3000.
   if (audioQuality > 3200) {
       audioQuality = 3200;
   }
-  
   // If the audio quality isn't already 0, slowly bring it down.
   if (audioQuality > 0) {
-      audioQuality -= ((1.0008 ** audioQuality) + 2)/1.5;
+      audioQuality -= ((1.0009 ** audioQuality) + 2)/1.5;
   }
-  
   // If you've maxed out the video quality, don't rotate the dial any further.
   if ((rotation + audioQuality) > 3200) {
     document.getElementById("audioDial").style.transform = "rotate(" + ((3200 / 13) - 120) + "deg)";
@@ -184,15 +164,12 @@ function audioQualityCheck() {
     // Otherwise, rotate the dial accordingly.
     document.getElementById("audioDial").style.transform = "rotate(" + (((rotation + audioQuality) / 13) - 120) + "deg)";
   }
-  
   // Get the embedded YouTube player and tell it to change the volume accordingly.
   player.setVolume(((rotation + audioQuality)/26));
   document.getElementById("audioNoise").play();
-  
   // Get the audio noise and set it's volume accordingly.
-  document.getElementById("audioNoise").volume = (clamp((1 - ((rotation + audioQuality)/2600)), 0, 1) / 4)
+  document.getElementById("audioNoise").volume = (clamp((1 - ((rotation + audioQuality)/2600)), 0, 1) / 4);
 }
-
 // Get an element and return it's rotation from 0 - 360 degrees
 function getRotation(target) {
     targetRotation = parseInt(target.style.transform.replace(/rotate\(|deg\)/g, ""));
@@ -200,22 +177,23 @@ function getRotation(target) {
         targetRotation = Math.abs(targetRotation += 360);
     }
     if (targetRotation > 360) {
-        targetRotation -= 360
+        targetRotation -= 360;
     }
     return targetRotation;
 }
-
 // Thanks CAFxX of Stack Overflow!
 // Clamp a number to [min - max] and return it.
 function clamp(number, min, max) {
     return Math.max(min, Math.min(number, max));
 }
-
 // Slowly build up pressure
 function pressureBuildup() {
   // Increase the pressure again in ~1/30 secs.
   var pressureTimeout = setTimeout(pressureBuildup, 33);
-  
+  // Make sure the pressure isn't below 0.
+  if (pressure < 0) {
+    pressure = 0;
+  }
   // If the user is attempting to vent pressure, decrease the pressure.
   if (pressureVenting === true) {
     if (pressure > 0) {
@@ -233,14 +211,14 @@ function pressureBuildup() {
     pressure = 0;
   }
   if (overheating === false) {
-    if (pressure >= 180) {
+    if (pressure >= 200) {
       overheating = true;
-      document.getElementById("overheatLight").src = "assets/overheat.png"
+      document.getElementById("overheatLight").src = "assets/overheat.png";
     }
   } else if (overheating === true) {
-    if (pressure < 180) {
+    if (pressure < 200) {
       overheating = false;
-      document.getElementById("overheatLight").src = "assets/noOverheat.png"
+      document.getElementById("overheatLight").src = "assets/noOverheat.png";
     }
   }
   // Set the pressure dial accordingly.
@@ -249,10 +227,11 @@ function pressureBuildup() {
 function blowUp() {
   // If the pressure gets too high, cause everything to explode.
   alert("You failed!");
-  
   // Reset everything.
   videoQuality = 0;
   audioQuality = 0;
-  
   // The current state of the function is a placeholder for a real explosion
+}
+function animateLoadButton() {
+  document.getElementById("loadButton").src = "assets/littleButton.png";
 }
